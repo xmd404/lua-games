f10cf6efe51991ae8912522944daf5efcc9426ec
Xavier Duncan <xduncan@pm.me>
2019-02-01 11:36:39 -0500

    write "Hello Pong" to screen

--- pong/main.lua
+++ pong/main.lua
@@ -6,7 +6,7 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 --[[
-  init game
+  init game, override default load behaviour
 ]]
 function love.load()
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -14,4 +14,17 @@ function love.load()
     resizable = false,
     vsync = true
   })
+end
+
+--[[
+  draw 'hello pong' to center of screen
+]]
+function love.draw()
+  love.graphics.printf(
+    'Hello Pong',
+    0,
+    WINDOW_HEIGHT / 2 - 6,
+    WINDOW_WIDTH,
+    'center'
+  )
 end

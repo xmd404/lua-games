1d8fa02e92a11ab65de35d4e566c9dc55b1c1e4c
Xavier Duncan <xduncan@pm.me>
2019-02-01 15:20:08 -0500

    add scoreboard and dynamic positioning of paddles

--- pong/main.lua
+++ pong/main.lua
@@ -17,7 +17,7 @@ function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
   -- add font objects
   smallFont = love.graphics.newFont('font.ttf', 8)
-  -- scoreFont = love.graphics.newFont('font.tff', 32)
+  scoreFont = love.graphics.newFont('font.ttf', 32)
   -- set active font to smallFont
   love.graphics.setFont(smallFont)
   -- init virtual resolution
@@ -64,6 +64,7 @@ function love.draw()
   -- -- clear screen with a specific color
   -- love.graphics.clear(40, 45, 52, 255)
   -- draw welcome text top center of screen
+  love.graphics.setFont(smallFont)
   love.graphics.printf(
     'Hello Pong',
     0, 
@@ -71,10 +72,22 @@ function love.draw()
     VIRTUAL_WIDTH,
     'center'
   )
+  -- draw player score left & right center of screen
+  love.graphics.setFont(scoreFont)
+  love.graphics.print(
+    tostring(playerOneScore),
+    VIRTUAL_WIDTH / 2 - 50,
+    VIRTUAL_HEIGHT / 3
+  )
+  love.graphics.print(
+    tostring(playerTwoScore),
+    VIRTUAL_WIDTH / 2 + 30,
+    VIRTUAL_HEIGHT / 3
+  )
   -- render 1st paddle (left side)
-  love.graphics.rectangle('fill', 10, 30, 5, 20)
+  love.graphics.rectangle('fill', 10, playerOnePosition, 5, 20)
   -- render 2nd paddel (right side)
-  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, playerTwoPosition, 5, 20)
   -- render ball (center)
   love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
   -- end animation

5504b244962b8947c1c7733e02a25ec09177d563
Xavier Duncan <xduncan@pm.me>
2019-02-07 08:17:32 -0500

    add parallax scrolling

--- flappy-bird/main.lua
+++ flappy-bird/main.lua
@@ -7,7 +7,15 @@ VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
 local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
 
 -- load game
 function love.load()
@@ -33,4 +41,20 @@ function love.keypressed(key)
   if key == 'escape' then
     love.event.quit()
   end
+end
+
+function love.update(dt)
+  backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+    % BACKGROUND_LOOPING_POINT
+    -- ground.png is consistent enough to not need a loop point
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+    % VIRTUAL_WIDTH
+end
+
+-- render to screen
+function love.draw()
+  push:start()
+    love.graphics.draw(background, -backgroundScroll, 0)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+  push:finish()
 end

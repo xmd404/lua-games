7b4c852c34260d3bbffe5c47f31ac353081bc4fb
Xavier Duncan <xduncan@pm.me>
2019-02-07 07:46:45 -0500

    add dynamic game canvas

--- flappy-bird/main.lua
+++ flappy-bird/main.lua
@@ -8,3 +8,22 @@ VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
 local ground = love.graphics.newImage('ground.png')
+
+-- load game
+function love.load()
+  -- remove bluriness on resize
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  -- set game title
+  love.window.setTitle('Flappy Bird')
+  -- init game screen
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    vsync = true,
+    fullscreen = false,
+    resizable = true
+  })
+end
+
+-- dynamic game canvas
+function love.resize(w, h)
+  push:resize(w, h)
+end

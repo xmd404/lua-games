6cdcd91479c17967580de2c0844e42cb02ca7f75
Xavier Duncan <xduncan@pm.me>
2019-02-06 22:00:30 -0500

    add FPS display

--- pong/main.lua
+++ pong/main.lua
@@ -121,6 +121,15 @@ function love.draw()
   love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, p2Position, 5, 20)
   -- render ball (center)
   love.graphics.rectangle('fill', ballX, ballY, 4, 4)
+  -- display FPS
+  displayFPS()
   -- end animation
   push:apply('end')
+end
+
+-- renders current FPS in top left corner
+function displayFPS()
+  love.graphics.setFont(smallFont)
+  love.graphics.setColor(0, 255, 0, 255)
+  love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
 end
